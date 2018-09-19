bb072d9fd48200ab06b495f56b65d93c15346f31
PT-EDU-MJEREZ\chema <josem32832@gmail.com>
2018-09-19 21:00:12 +0200

    Commit
    Chema

---
+++ ninety-nine-scala-problems/src/working_with_lists/Problems.scala
@@ -0,0 +1,33 @@
+package working_with_lists
+
+object Problems extends App{
+  /**
+   * P01 (*) Find the last element of a list.
+   * Example:
+   * scala> last(List(1, 1, 2, 3, 5, 8))
+   * res0: Int = 8
+   * [T] ==> method can accept type String, Int, Double ....
+   */
+  def last[T](l: List[T]): T = {
+    l match {
+      case (h :: Nil) => h 
+      case (h :: tail) => last(tail)      
+      case _ => throw new NoSuchElementException
+    }    
+  }
+  
+  def lastBuiltIn[T](l: List[T]): T = {
+    l.last
+  }
+  println(last(List(1, 1, 2, 3, 5, 8)))
+  println(lastBuiltIn(List(1, 1, 2, 3, 5, 8)))
+  println(last(List()))
+  println(lastBuiltIn(List()))
+  
+  /**
+   * P02 (*) Find the last but one element of a list.
+   * Example:
+   * scala> penultimate(List(1, 1, 2, 3, 5, 8))
+   * res0: Int = 5
+   */
+}
---
+++ ninety-nine-scala-problems/src/working_with_lists/readme.md
@@ -0,0 +1,5 @@
+In Scala, lists are objects of type List[A], where A can be any type. Lists are effective for many recursive algorithms, because it's easy to add elements to the head of a list, and to get the tail of the list, which is everything but the first element.
+
+The solutions to the problems in this section will be in objects named after the problems (P01, P02, etc.). You can compile the source files with scalac and thereafter use import to bring the functions into scope. Some of the problems can be solved easily by using imported solutions to previous problems.
+
+In many cases, there's more than one reasonable approach. 
