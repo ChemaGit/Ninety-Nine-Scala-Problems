a6cceb11d7d51c9d3ad0582c7e38e163386ebb26
Chema <chema@localhost.chema>
2019-08-22 16:53:31 +0200

    22082019

--- ninety-nine-scala-problems/src/working_with_lists/Problems.scala
+++ ninety-nine-scala-problems/src/working_with_lists/Problems.scala
@@ -572,5 +572,38 @@ object Problems extends App{
   }
   println("P025: " + randomPermute(List(1,2,3,4,5,6)))
   println("P025: " + randomPermute(List('a, 'b, 'c, 'd, 'e, 'f)))
+
+  /**
+    * P26 (**) Generate the combinations of K distinct objects chosen from the N elements of a list.
+    * In how many ways can a committee of 3 be chosen from a group of 12 people? We all know that there are C(12,3) = 220 possibilities (C(N,K) denotes the well-known binomial coefficient).
+    * For pure mathematicians, this result may be great. But we want to really generate all the possibilities.
+    * Example:
+    *
+    * scala> combinations(3, List('a, 'b, 'c, 'd, 'e, 'f))
+    * res0: List[List[Symbol]] = List(List('a, 'b, 'c), List('a, 'b, 'd), List('a, 'b, 'e), ...
+    */
+  def combinations[T](n: Int, l: List[T]): List[List[T]] = {
+    l.permutations.flatMap(x => {val(a,b)=x.splitAt(n);Set(a.toSet,b.toSet)}).filter(l => l.size == n).toSet.map( (s: Set[T]) => s.toList).toList
+  }
+  println("P026: " + combinations(3, List(1,2,3,4,5,6)))
+  println("P026: " + combinations(3, List('a, 'b, 'c, 'd, 'e, 'f)))
+  /**
+    * P27 (**) Group the elements of a set into disjoint subsets.
+    * a) In how many ways can a group of 9 people work in 3 disjoint subgroups of 2, 3 and 4 persons? Write a function that generates all the possibilities.
+    * Example:
+    *
+    * scala> group3(List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+    * res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David, Evi), List(Flip, Gary, Hugo, Ida)), ...
+    * b) Generalize the above predicate in a way that we can specify a list of group sizes and the predicate will return a list of groups.
+    *
+    * Example:
+    *
+    * scala> group(List(2, 2, 5), List("Aldo", "Beat", "Carla", "David", "Evi", "Flip", "Gary", "Hugo", "Ida"))
+    * res0: List[List[List[String]]] = List(List(List(Aldo, Beat), List(Carla, David), List(Evi, Flip, Gary, Hugo, Ida)), ...
+    * Note that we do not want permutations of the group members; i.e. ((Aldo, Beat), ...) is the same solution as ((Beat, Aldo), ...).
+    * However, we make a difference between ((Aldo, Beat), (Carla, David), ...) and ((Carla, David), (Aldo, Beat), ...).
+    *
+    * You may find more about this combinatorial problem in a good book on discrete mathematics under the term "multinomial coefficients".
+    */
 }
 
